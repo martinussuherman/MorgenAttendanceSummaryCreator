023d1d0a6252eac45c65b9601f34024f49a20937
Martinus Suherman <martinus.suherman@gmail.com>
2024-07-15 13:39:14 +0700

    Add start and end date information model

---
+++ Models/StartAndEndDateInfo.cs
@@ -0,0 +1,8 @@
+namespace MorgenAttendanceSummaryCreator.Models;
+
+public class StartAndEndDateInfo
+{
+    public DateTime StartDate { get; set; } = DateTime.MinValue;
+
+    public DateTime EndDate { get; set; } = DateTime.MinValue;
+}
