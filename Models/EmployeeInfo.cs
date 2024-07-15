e5f900d8945e008afce5902cb6f5e2a8b8f69e6b
Martinus Suherman <martinus.suherman@gmail.com>
2024-07-15 13:37:23 +0700

    Add employee information model

---
+++ Models/EmployeeInfo.cs
@@ -0,0 +1,12 @@
+namespace MorgenAttendanceSummaryCreator.Models;
+
+public class EmployeeInfo
+{
+    public string Id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Department { get; set; } = string.Empty;
+
+    public string Shift { get; set; } = string.Empty;
+}
