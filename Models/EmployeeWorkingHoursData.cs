f1617eb730e488141b45a2bb01d40b38a9a4f1b2
Martinus Suherman <martinus.suherman@gmail.com>
2024-08-30 14:58:25 +0700

    Update employee working hour data model - add count of no time in and out

--- Models/EmployeeWorkingHoursData.cs
+++ Models/EmployeeWorkingHoursData.cs
@@ -5,4 +5,8 @@ public class EmployeeWorkingHoursData
     public EmployeeInfo EmployeeInfo { get; set; } = new EmployeeInfo();
 
     public List<WorkingHoursData> WorkingHoursData { get; set; } = [];
+
+    public int NoTimeInCount { get; set; } = 0;
+
+    public int NoTimeOutCount { get; set; } = 0;
 }
